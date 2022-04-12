6d99025dd47dc224a5dbc9a34cd220f31a8ede04
charris1995 <charris@quickcustomintelligence.com>
2022-04-12 11:17:38 -0700

    Removed test vars

---
+++ p5250Wrapper.py
@@ -0,0 +1,13 @@
+
+import p5250
+
+
+class p5250WebAPI:
+    def __init__(self):
+        self.my_client = p5250.P5250Client(hostName='', path='C:\\Program Files\\wc3270\\', codePage='cp037',hostPort='', verifyCert='no', enableTLS='yes')
+        
+
+
+
+        
+
